998bfe7963cb31d848049a1173c612828ea52c99
mm222qy <mm222qy@student.lnu.se>
2014-10-09 12:43:52 +0200

    Finished

---
+++ 1DV402.S2.L1A/1DV402.S2.L1A/SecretNumber.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1DV402.S2.L1A
+{
+    class SecretNumber
+    {
+
+        private int _count;
+        private int _number;
+        public const int MaxNumberOfGuesses = 7;
+        
+       public SecretNumber(){
+           Random rnd = new Random();
+           _number = rnd.Next(1, 101);
+           
+    }
+
+        public void Initialize()
+        {
+            
+            _count = 0;
+
+
+        }
+        public bool MakeGuess(int number)
+        {
+            if (_count >= MaxNumberOfGuesses)
+            {
+                throw new ApplicationException();
+            }
+
+            if (number < 1 || number > 100)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            _count++;
+
+            
+            if (number == _number)
+            {
+                Console.WriteLine("RÄTT GISSAT. Du klarade det på {0} försök.", _count);
+                return true;
+            }
+            else if (number > _number)
+            {
+                Console.WriteLine("{0} är för högt. Du har {1} gissningar kvar.", number, MaxNumberOfGuesses - _count);
+            }
+            else if (number < _number)
+            {
+                Console.WriteLine("{0} är för lågt. Du har {1} gissningar kvar.", number, MaxNumberOfGuesses - _count);
+            }
+            if (MaxNumberOfGuesses == _count)
+            {
+                Console.WriteLine("Det hemliga talet är {0} ", _number);
+            }
+            return false;
+        }
+        
+
+    }
+}
